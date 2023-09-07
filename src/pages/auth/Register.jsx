108c9cf7cdaf91e4f6d2c532b6a3b63e1a4e3662
Jeetesh <jeeteshkumar45@gmail.com>
2023-09-08 01:41:15 +0530

    Register

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import registerImage from "../../assets/register.png";
 import "./Auth.css";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <section className="container auth">
+      <div className="form">
+        <h2>Register</h2>
+        <form>
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <input type="password" placeholder="Confirm Password" required />
+          <button className="--btn --btn-primary --btn-block">Register</button>
+        </form>
+        <span className="register">
+          <h6>Already an account?</h6>
+          <Link to="/login">Login</Link>
+        </span>
+      </div>
+      <div className="img">
+        <img src={registerImage} alt="Login" width="400" />
+      </div>
+    </section>
+  );
 };
 
 export default Register;
