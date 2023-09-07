88de5239ac8d21a37832ffdd639bd6dec9bafcdc
Jeetesh <jeeteshkumar45@gmail.com>
2023-09-08 01:23:16 +0530

    Login Page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Auth.css";
 import loginImage from "../../assets/login.png";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
   return (
@@ -13,8 +15,15 @@ const Login = () => {
         <form>
           <input type="email" placeholder="Email" required />
           <input type="password" placeholder="Password" required />
-          <button className="--btn">Login</button>
+          <button className="--btn --btn-primary --btn-block">Login</button>
+          <div className="link">
+            <Link to="/reset">Forgot Password</Link>
+          </div>
+          <h5>-- or --</h5>
         </form>
+        <button className="--btn --btn-danger --btn-block">
+          <FaGoogle color="#fff" /> Login with Google
+        </button>
       </div>
     </section>
   );
