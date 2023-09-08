28122e112ecc41f150ee05ae413f18e25ff933c9
Jeetesh <jeeteshkumar45@gmail.com>
2023-09-09 01:43:23 +0530

    Loader Components

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Loader.css";
+import loaderImage from "../../assets/loader.gif";
+import ReactDOM from "react-dom";
+const Loader = () => {
+  return ReactDOM.createPortal(
+    <div className="wrapper">
+      <div className="loader">
+        <img src={loaderImage} alt="Loading..." />
+      </div>
+    </div>,
+    document.getElementById("loading")
+  );
+};
+
+export default Loader;
